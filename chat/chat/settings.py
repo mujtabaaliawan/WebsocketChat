7dc3bea0e422db01116eb3fb1b4019f7ac04cc0d
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2023-01-02 12:57:12 +0500

    settings and docker-compose updated

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -94,16 +94,12 @@ CHANNEL_LAYERS = {
 
 
 DATABASES = {
-    # 'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'chatty',
+        'NAME': 'postgres',
         'USER': 'mujtaba',
         'PASSWORD': 'maz',
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': '5432',
     }
 }
