1d92e1ad5a8fae9af87ba8dbf35d338bd17372da
Mujtaba Ali <117171332+mujtabaaliawan@users.noreply.github.com>
2023-01-10 15:25:28 +0500

    code updated

--- chat/message/consumers.py
+++ chat/message/consumers.py
@@ -18,24 +18,23 @@ class ChatConsumer(WebsocketConsumer):
         user = self.scope["user"]
         box_id = self.scope["url_route"]["kwargs"]["box_id"]
         self.room_group_name = "chat_%s" % box_id
-
-        self.update_box_detail_enter(user, box_id)
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
         )
 
         self.accept()
+        self.update_box_detail_enter(user, box_id)
 
     def disconnect(self, close_code):
         # Leave room group
         user = self.scope["user"]
         box_id = self.scope["url_route"]["kwargs"]["box_id"]
-        self.update_box_detail_exit(user, box_id)
 
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
+        self.update_box_detail_exit(user, box_id)
 
     # Receive message from WebSocket
     def receive(self, text_data):
@@ -122,7 +121,9 @@ class ChatConsumer(WebsocketConsumer):
         old_box_detail["online_chatters"].append(chatter_id)
 
         old_box_detail.setdefault("users_sessions_details", {})
+        old_box_detail.setdefault("last_session_details", {})
         old_box_detail["users_sessions_details"].setdefault(chatter_id_str, {})
+        old_box_detail["last_session_details"].setdefault(chatter_id_str, {})
 
         old_box_detail.setdefault("highest_average_session_duration", "00:00:00")
         old_box_detail.setdefault("highest_average_session_duration_chatter_id", 0)
@@ -147,8 +148,11 @@ class ChatConsumer(WebsocketConsumer):
         new_session_number = previous_session_number + 1
         new_session_number_str = str(new_session_number)
         old_box_detail["users_sessions_details"][chatter_id_str]["session"].setdefault(new_session_number_str, {})
+        login_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         old_box_detail["users_sessions_details"][chatter_id_str]["session"][new_session_number_str].setdefault(
-            "login_time", datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+            "login_time", login_time)
+        old_box_detail["last_session_details"][chatter_id_str]["login_time"] = login_time
+
         serializer = BoxSerializer(box, data={"box_detail": old_box_detail}, partial=True)
         if serializer.is_valid():
             serializer.save()
@@ -164,10 +168,12 @@ class ChatConsumer(WebsocketConsumer):
 
         new_session_number_str = list(old_box_detail["users_sessions_details"][chatter_id_str]["session"])[-1]
 
+        logout_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         old_box_detail["users_sessions_details"][chatter_id_str]["session"][new_session_number_str].setdefault(
-            "logout_time", datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+            "logout_time", logout_time)
+        old_box_detail["last_session_details"][chatter_id_str]["logout_time"] = logout_time
 
-        #session_duration
+        # session_duration
         login_time = datetime.datetime.strptime(
             old_box_detail["users_sessions_details"][chatter_id_str]["session"][new_session_number_str][
                 "login_time"], '%d/%m/%Y %H:%M:%S')
@@ -175,24 +181,69 @@ class ChatConsumer(WebsocketConsumer):
             old_box_detail["users_sessions_details"][chatter_id_str]["session"][new_session_number_str][
                 "logout_time"], '%d/%m/%Y %H:%M:%S')
         duration = logout_time - login_time
+        duration_str = str(duration)
         old_box_detail["users_sessions_details"][chatter_id_str]["session"][new_session_number_str].setdefault(
-            "duration", str(duration))
+            "duration", duration_str)
+        old_box_detail["last_session_details"][chatter_id_str]["duration"] = duration_str
 
-        #average_session_duration
+        # highest_duration
+        duration_seconds = self.convert_time_string_to_total_seconds(duration_str)
+
+        highest_session_duration_seconds = self.convert_time_string_to_total_seconds(
+            old_box_detail["highest_session_duration"])
+        if duration_seconds > highest_session_duration_seconds:
+            old_box_detail["highest_session_duration"] = duration_str
+            old_box_detail["highest_session_duration_chatter_id"] = chatter_id_str
+
+        # lowest_duration
+        lowest_session_duration_seconds = self.convert_time_string_to_total_seconds(
+            old_box_detail["lowest_session_duration"])
+        if duration_seconds < lowest_session_duration_seconds:
+            old_box_detail["lowest_session_duration"] = duration_str
+            old_box_detail["lowest_session_duration_chatter_id"] = chatter_id_str
+
+        # average_session_duration
         previous_average_duration = old_box_detail["users_sessions_details"][chatter_id_str]["average_session_duration"]
-
         old_average_duration_seconds = self.convert_time_string_to_total_seconds(time_string=previous_average_duration)
         old_session_number = int(new_session_number_str) - 1
-        total_sum = (old_average_duration_seconds * old_session_number) + duration.total_seconds()
+        total_sum = (old_average_duration_seconds * old_session_number) + self.convert_time_string_to_total_seconds(
+            duration_str)
         new_average_session_duration_seconds = float(total_sum / int(new_session_number_str))
-        new_average_session_duration = str(datetime.timedelta(seconds=new_average_session_duration_seconds))
-        old_box_detail["users_sessions_details"][chatter_id_str]["average_session_duration"] = str(
-            new_average_session_duration)
+        new_average_session_duration_str = str(datetime.timedelta(seconds=new_average_session_duration_seconds))
+        old_box_detail["users_sessions_details"][chatter_id_str]["average_session_duration"] =\
+            new_average_session_duration_str
 
-        #highest_average_session_duration
+        # highest_average_session_duration
         highest_average_session_duration = old_box_detail["highest_average_session_duration"]
-        highest_average_session_duration_chatter_id = old_box_detail["highest_average_session_duration_chatter_id"]
+        if old_box_detail["highest_average_session_duration_chatter_id"] == chatter_id_str:
+            self.find_new_highest_average_duration(old_box_detail)
+        elif new_average_session_duration_seconds > self.convert_time_string_to_total_seconds(
+                highest_average_session_duration):
+            old_box_detail["highest_average_session_duration"] = new_average_session_duration_str
+            old_box_detail["highest_average_session_duration_chatter_id"] = chatter_id_str
 
+        # lowest_average_session_duration
+        lowest_average_session_duration = old_box_detail["lowest_average_session_duration"]
+        if old_box_detail["lowest_average_session_duration_chatter_id"] == chatter_id_str:
+            self.find_new_lowest_average_duration(old_box_detail)
+        elif new_average_session_duration_seconds < self.convert_time_string_to_total_seconds(
+                lowest_average_session_duration):
+            old_box_detail["lowest_average_session_duration"] = new_average_session_duration_str
+            old_box_detail["lowest_average_session_duration_chatter_id"] = chatter_id_str
+
+        print(old_box_detail)
+        serializer = BoxSerializer(box, data={"box_detail": old_box_detail}, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+
+    def convert_time_string_to_total_seconds(self, time_string):
+        hours, minutes, seconds = time_string.split(':')
+        total_seconds = (int(hours) * 3600) * (int(minutes) * 60) + float(seconds)
+        return total_seconds
+
+    def find_new_highest_average_duration(self, old_box_detail):
+        highest_average_session_duration = "00:00:00"
+        highest_average_session_duration_chatter_id = '0'
         for chatters in old_box_detail["users_sessions_details"]:
             average_session_duration = self.convert_time_string_to_total_seconds(
                 old_box_detail["users_sessions_details"][chatters]["average_session_duration"])
@@ -207,9 +258,9 @@ class ChatConsumer(WebsocketConsumer):
         old_box_detail["highest_average_session_duration"] = highest_average_session_duration
         old_box_detail["highest_average_session_duration_chatter_id"] = highest_average_session_duration_chatter_id
 
-        #lowest_average_session_duration
-        lowest_average_session_duration = old_box_detail["lowest_average_session_duration"]
-        lowest_average_session_duration_chatter_id = old_box_detail["lowest_average_session_duration_chatter_id"]
+    def find_new_lowest_average_duration(self, old_box_detail):
+        lowest_average_session_duration = "23:59:59"
+        lowest_average_session_duration_chatter_id = '0'
 
         for chatters in old_box_detail["users_sessions_details"]:
 
@@ -225,16 +276,3 @@ class ChatConsumer(WebsocketConsumer):
 
         old_box_detail["lowest_average_session_duration"] = lowest_average_session_duration
         old_box_detail["lowest_average_session_duration_chatter_id"] = lowest_average_session_duration_chatter_id
-
-        #highest_duration
-        #lowest_duration
-        #all_users_last_sessions_details
-        print(old_box_detail)
-        serializer = BoxSerializer(box, data={"box_detail": old_box_detail}, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-
-    def convert_time_string_to_total_seconds(self, time_string):
-        hours, minutes, seconds = time_string.split(':')
-        total_seconds = (int(hours)*3600) * (int(minutes)*60) + float(seconds)
-        return total_seconds
