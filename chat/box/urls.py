e501fb99d3871371a2811021714971af14d35fd7
Mujtaba Ali <117171332+mujtabaaliawan@users.noreply.github.com>
2023-01-10 15:46:01 +0500

    view and url adjusted

--- chat/box/urls.py
+++ chat/box/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('box', views.BoxCreateList.as_view(), name='box_list_new'),
     path('box/<int:pk>', views.BoxUpdate.as_view(), name='box_update'),
     path('induction/<int:pk>', views.InductionUpdate.as_view(), name='induction'),
+    path('detail/<int:box_id>', views.BoxDetail.as_view(), name='box_detail'),
 ]
--- chat/box/views/views.py
+++ chat/box/views/views.py
@@ -1,7 +1,10 @@
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 from box.models import Box
 from box.serializers import BoxSerializer, InductionSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from box.permissions import box_update_permission
+from rest_framework.views import APIView
 
 
 class BoxCreateList(ListCreateAPIView):
@@ -19,3 +22,12 @@ class BoxUpdate(RetrieveUpdateAPIView):
 class InductionUpdate(RetrieveUpdateAPIView):
     queryset = Box.objects.all()
     serializer_class = InductionSerializer
+
+
+class BoxDetail(APIView):
+
+    parser_classes = [JSONParser]
+
+    def get(self, request, *args, **kwargs):
+        box_id = self.kwargs["box_id"]
+        return JsonResponse(data=Box.objects.get(id=box_id).box_detail)
